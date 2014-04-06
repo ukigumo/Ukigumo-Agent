bd268fea7dc197a8b23678d4b0d9eb15b1713c7d
moznion <moznion@gmail.com>
2014-04-06 23:48:30 +0900

    Checking in changes prior to tagging of version v0.1.3.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 87612f6..171fce2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Ukigumo::Agent
    
     {{$NEXT}}
    
    +v0.1.3 2014-04-06T14:48:07Z
    +
    +    - Update a dependency to Ukigumo::Client for function of timeout
    +
     v0.1.2 2014-04-05T05:48:59Z
    
         - Change timeout function to be more safety (moznion)

--- lib/Ukigumo/Agent.pm
+++ lib/Ukigumo/Agent.pm
@@ -2,7 +2,7 @@ package Ukigumo::Agent;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v0.1.2");
+use version; our $VERSION = version->declare("v0.1.3");
 use parent qw(Amon2 Amon2::Web);
 
 sub config { +{ } }
