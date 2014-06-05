a22bc47c1d95e4584b20c7d95da902aea49520ed
moznion <moznion@gmail.com>
2014-06-05 10:06:40 +0900

    Fix the contradiction of version description of perl runtime

--- lib/Ukigumo/Agent.pm
+++ lib/Ukigumo/Agent.pm
@@ -1,7 +1,7 @@
 package Ukigumo::Agent;
 use strict;
 use warnings;
-use 5.008005;
+use 5.010001;
 use version; our $VERSION = version->declare("v0.1.6");
 use parent qw(Amon2 Amon2::Web);
 use Carp ();
